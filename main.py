e9a3d8cf0a11f728e49800aa79ad74f726bc666c
Marina Pérez <marina1611@gmail.com>
2022-11-21 19:19:15 +0100

    project

--- main.py
+++ main.py
@@ -10,12 +10,34 @@ conexion = psycopg2.connect(
     password='admin',
     database='postgres'
 )
-conexion.autocommit = True
+#conexion.autocommit = True
 cursor = conexion.cursor()
 
-print('Conexión realizada con éxito')
+
 #tabla = "CREATE TABLE flor (Nombre STRING(100) NOT NULL, Tipo STRING(100) NOT NULL);"
 #cursor.execute(tabla)
 
 #query = "INSERT INTO flor (Nombre, Tipo) VALUES ({response['name']}, {response['type']})"
 #cursor.execute(query)
+name_Table = "flor"
+
+
+# Create table statement
+
+sqlCreateTable = "create table "+name_Table+" (name varchar(128), type varchar(256));"
+
+# Create a table in PostgreSQL database
+
+cursor.execute(sqlCreateTable)
+conexion.commit()
+
+
+query = "INSERT INTO flor (name, type) VALUES (1,2)"
+
+cursor.execute(query)
+conexion.commit()
+
+tables = cursor.fetchall()
+for table in tables:
+
+    print(table)
